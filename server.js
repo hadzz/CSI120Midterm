a5f9a000aad2c1d4cc2681c8aae4be60b1ed8035
OpenShift guest <ajhmonarchs95@comcast.net>
2013-11-18 18:56:18 -0500

    fixing server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var weather = require('weather')
 var app = express();
 
 
 app.get('/', function(req, res){
   res.send('hello world');
+  weather({location: 'Burlington, VT'}, function(data) {
+  if (data.temp > 30) {
+    console.log("Damn it's hot!");
+  }
 });
+});
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port);
