330b3d483ea889d24e5b999c6f3f1507b988378b
OpenShift guest <ajhmonarchs95@comcast.net>
2013-11-18 18:18:43 -0500

    updating server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
+var weather = require('weather');
+
 
 app.get('/', function(req, res){
+  weather();
   res.send('hello world');
+});
+
+weather({location: 'Melbourne'}, function(data) {
+  if (data.temp > 30) {
+    console.log("Damn it's hot!");
+  }
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port);
+
+
+
+
