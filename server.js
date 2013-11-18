71a3b186b8e8edf4665bedf6370de496861bea12
OpenShift guest <ajhmonarchs95@comcast.net>
2013-11-18 18:42:53 -0500

    fixing package.json refs

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 var express = require('express');
 var app = express();
-var weather = require('weather');
 
 
 app.get('/', function(req, res){
   res.send('hello world');
-});
-
-weather({location: 'Melbourne'}, function(data) {
-  if (data.temp > 30) {
-    console.log("Damn it's hot!");
-  }
 });
 
 var port = process.env.PORT || 5000;
